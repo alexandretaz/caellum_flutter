eceb0c2ece115376465bb701486c27f6c9a26c45
Alexandre dos Santos Andrade <alexandretaz@gmail.com>
2021-05-04 17:39:58 -0300

    Execução do Exercício 2.5 com a consolidação da aparência conforme o Wireframe, utilizando Material Design

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,32 @@ import 'package:flutter/material.dart';
 void main() =>
     runApp(
         MaterialApp(
-          home: Scaffold(appBar: AppBar(
-            title: Text('Transferências'),
-          ),
-            floatingActionButton: FloatingActionButton(
-              child: Icon(Icons.add),
+          home: Scaffold(
+            body: Column(
+              children: <Widget>[
+                Card(
+                  child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('100,00'),
+                  subtitle: Text('1000'),
+                  ),
+                ),
+                Card(
+                  child: ListTile(
+                    leading: Icon(Icons.monetization_on),
+                    title: Text('500,00'),
+                    subtitle: Text('1500'),
+                  ),
+                ),
+              ],
             ),
-          ),
+            appBar: AppBar(
+        title: Text('Transferência'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+          )
         )
     );
 

eceec6e65b74ed534dd2ed1289ffa26c1c9995a5
Alexandre dos Santos Andrade <alexandretaz@gmail.com>
2021-05-04 20:20:26 -0300

    Execução do Exercício 2.7 com a consolidação da aparência conforme o Wireframe, utilizando Material Design

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,9 @@ void main() =>
     runApp(
         MaterialApp(
           home: Scaffold(
-            body: Column(
-              children: <Widget>[
-                Card(
-                  child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('100,00'),
-                  subtitle: Text('1000'),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    leading: Icon(Icons.monetization_on),
-                    title: Text('500,00'),
-                    subtitle: Text('1500'),
-                  ),
-                ),
-              ],
-            ),
+            body:ListarTransferencia(),
             appBar: AppBar(
-        title: Text('Transferência'),
+              title: Text('Minhas Trasnferências')
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -36,3 +19,44 @@ void main() =>
 {
 
 }*/
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+
+}
+
+class ListarTransferencia extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+    children:<Widget>[
+      ItemTransferencia(Transferencia(100.00, 1000)),
+      ItemTransferencia(Transferencia(200.00, 1001)),
+      ItemTransferencia(Transferencia(300.00, 2000)),
+    ],
+    );
+
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text (_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      )
+
+    );
+  }
+}

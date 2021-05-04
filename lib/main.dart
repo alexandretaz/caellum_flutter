4575a5749ac2652ccf4a9b9d965c1cccfbb56aec
Alexandre dos Santos Andrade <alexandretaz@gmail.com>
2021-05-04 16:18:50 -0300

    Execução do exercício 2.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Column(
-      children: <Widget>[
-        Text(
-          'Olá Mundo Flutter',
-          textDirection: TextDirection.ltr,
-        ),
-        Text(
-          "Outra mensagem para o usuário",
-          textDirection: TextDirection.ltr,
-        ),
-        Text(
-          "Mais uma mensagem para o usuário",
-          textDirection: TextDirection.ltr,
-        ),
-        Column(
-          children: <Widget>[
-            Text(
-              'Quarta mensagem',
-              textDirection: TextDirection.ltr,
+void main() =>
+    runApp(
+        MaterialApp(
+          home: Scaffold(appBar: AppBar(
+            title: Text('Transferências'),
+          ),
+            floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.add),
             ),
-          ],
+          ),
         )
-      ],
-    ));
+    );
 
 /*class byteBankApp extends StatelessWidget
 {
